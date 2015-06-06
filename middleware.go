30bbe4bfcd0138b960aa28192a0883afaf7545f5
Harsh <hkundra@gmail.com>
2015-06-06 22:29:19 +0530

    Added few standard modules

---
+++ middleware.go
@@ -0,0 +1,42 @@
+package aqua
+
+import (
+	"github.com/thejackrabbit/aero/panik"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func ModAccessLog(path string) func(http.Handler) http.Handler {
+
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	panik.On(err)
+	l := log.New(f, "", log.LstdFlags)
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			next.ServeHTTP(w, r)
+			l.Printf("%s %s %.3f", r.Method, r.RequestURI, time.Since(start).Seconds())
+		})
+	}
+}
+
+func ModSlowLog(path string, msec int) func(http.Handler) http.Handler {
+
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	panik.On(err)
+	l := log.New(f, "", log.LstdFlags)
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			next.ServeHTTP(w, r)
+			dur := time.Since(start).Seconds() - float64(msec)/1000.0
+			if dur > 0 {
+				l.Printf("%s %s %.3f", r.Method, r.RequestURI, time.Since(start).Seconds())
+			}
+		})
+	}
+}
