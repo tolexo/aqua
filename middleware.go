c7f4aae5a9db1cd3bde6e27e9cec2c2af99d8349
Harsh K <mak@HK.local>
2016-03-14 18:09:34 +0530

    dblog middleware

--- middleware.go
+++ middleware.go
@@ -1,11 +1,13 @@
 package aqua
 
 import (
-	"github.com/tolexo/aero/panik"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/tolexo/aero/db/orm"
+	"github.com/tolexo/aero/panik"
 )
 
 func ModAccessLog(path string) func(http.Handler) http.Handler {
@@ -37,6 +39,24 @@ func ModSlowLog(path string, msec int) func(http.Handler) http.Handler {
 			if dur > 0 {
 				l.Printf("%s %s %.3f", r.Method, r.RequestURI, time.Since(start).Seconds())
 			}
+		})
+	}
+}
+
+func ModDBStatsLog(path string) func(http.Handler) http.Handler {
+
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	panik.On(err)
+	l := log.New(f, "", log.LstdFlags)
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mastr := orm.Get(true)
+			dbref := mastr.DB()
+			before := dbref.Stats().OpenConnections
+			next.ServeHTTP(w, r)
+			after := dbref.Stats().OpenConnections
+			l.Printf("%s %s Conn::before:%d after:%d diff:%d", r.Method, r.RequestURI, before, after, (after - before))
 		})
 	}
 }
